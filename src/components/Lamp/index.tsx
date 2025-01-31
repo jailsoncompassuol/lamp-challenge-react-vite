7b26d9ccf822359cead76bec7fc91e559ff8b88f
jailsoncompassuol <jailson.souza@compasso.com.br>
2025-01-30 22:55:06 -0300

    feat (lamp): created componet

---
+++ src/components/Lamp/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+import LampON from '../../assets/lamp-on.png'
+import LampOFF from '../../assets/lamp-off.png'
+import { useMemo, useState } from 'react'
+
+export const Lamp = () => {
+
+    const [isOn, setIsOn] = useState(false)
+    const lampImage = useMemo(() => isOn ? LampON : LampOFF, [isOn])
+
+    return (
+        <img width={150} height={250} src={lampImage} alt='lamp' onClick={() => setIsOn(!isOn)} />
+    )
+}
