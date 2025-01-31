1d86d01d7737c3921c2b1475620e2a2dfe638d81
jailsoncompassuol <jailson.souza@compasso.com.br>
2025-01-31 00:08:50 -0300

    refect (lamp lazy): remove comments

--- src/components/Lamp/index.tsx
+++ src/components/Lamp/index.tsx
@@ -1,13 +1,18 @@
-import LampON from '../../assets/lamp-on.png'
-import LampOFF from '../../assets/lamp-off.png'
-import { useMemo, useState } from 'react'
+import LampON from "../../assets/lamp-on.png";
+import LampOFF from "../../assets/lamp-off.png";
+import { useMemo, useState } from "react";
 
 export const Lamp = () => {
+  const [isOn, setIsOn] = useState(false);
+  const lampImage = useMemo(() => (isOn ? LampON : LampOFF), [isOn]);
 
-    const [isOn, setIsOn] = useState(false)
-    const lampImage = useMemo(() => isOn ? LampON : LampOFF, [isOn])
-
-    return (
-        <img width={150} height={250} src={lampImage} alt='lamp' onClick={() => setIsOn(!isOn)} />
-    )
-}
+  return (
+    <img
+      width={150}
+      height={250}
+      src={lampImage}
+      alt="lamp"
+      onClick={() => setIsOn(!isOn)}
+    />
+  );
+};
--- src/components/LampLaze/index.tsx
+++ src/components/LampLaze/index.tsx
@@ -1,21 +1,18 @@
 import LampON from "../../assets/lamp-on.png";
 import LampOFF from "../../assets/lamp-off.png";
 import { useEffect, useMemo, useState } from "react";
-
 interface LampLazyProps {
   isOn: boolean;
   onCountClickOn: (count: number) => void;
 }
 
 export const LampLazy = ({ isOn, onCountClickOn }: LampLazyProps) => {
-
-  const [countClickOn, setCountClickOn] = useState(0)
-
+  const [countClickOn, setCountClickOn] = useState(0);
   const lampImage = useMemo(() => (isOn ? LampON : LampOFF), [isOn]);
 
   useEffect(() => {
-    onCountClickOn(countClickOn)
-  }, [countClickOn, onCountClickOn])
+    onCountClickOn(countClickOn);
+  }, [countClickOn, onCountClickOn]);
 
   return (
     <img
