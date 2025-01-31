54d48ca4c430f469c17a9b1e280d80eb1ff12d4b
jailsoncompassuol <jailson.souza@compasso.com.br>
2025-01-30 22:54:48 -0300

    feat: startup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className="app-main">
+      <h1>Hello, Lamp!</h1>
+      <section className="app-lamps">
+  
+      </section>
+    </main>
+  );
+};
+
+export default App;
